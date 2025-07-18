6770cc3fbe52c4287bf5243d89420107679e2de8
L'Envers du décor <lenversdudecorsainedecrime@gmail.com>
2025-07-18 21:41:00 +0200

    Update firebase.js
    test

--- js/firebase.js
+++ js/firebase.js
@@ -6,34 +6,25 @@ import {
   set,
   push,
   onValue,
-  update
+  update,
+  remove
 } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
 
-// 🔧 Configuration Firebase (ton projet actif)
+// Config Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBDxkMi1JwAKhR0VaA0cqXb3mLeHc-XZcw",
+  apiKey: "...",
   authDomain: "le-ballon-de-plomb.firebaseapp.com",
-  databaseURL: "https://le-ballon-de-plomb-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "le-ballon-de-plomb",
-  storageBucket: "le-ballon-de-plomb.firebasestorage.app",
-  messagingSenderId: "382972822900",
-  appId: "1:382972822900:web:79a25d551ef32c13c71821",
-  measurementId: "G-NF3LESNKFE"
+  databaseURL: "https://...default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
 
-// 🚀 Initialisation Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// 📦 Export des fonctions Firebase utiles
-export {
-  db,
-  ref,
-  set,
-  push,
-  onValue,
-  update
-};
-
 // Exporter tout ce dont on a besoin
 export { db, ref, set, push, onValue, update, remove };
+
